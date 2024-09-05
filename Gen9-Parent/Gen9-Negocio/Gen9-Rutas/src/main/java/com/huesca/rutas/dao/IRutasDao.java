87b7fcaef889ae626eab3da71fe42c908b7adefa
AlinHuesca <alinhuescagonzalez19@gmail.com>
2024-09-04 23:55:17 -0600

    Se agrego rutas

---
+++ Gen9-Parent/Gen9-Negocio/Gen9-Rutas/src/main/java/com/huesca/rutas/dao/IRutasDao.java
@@ -0,0 +1,9 @@
+package com.huesca.rutas.dao;
+
+import com.huesca.rutas.models.Ruta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRutasDao  extends JpaRepository<Ruta, Long>{
+
+
+}
---
+++ Gen9-Parent/Gen9-Negocio/Gen9-Rutas/src/test/java/com/huesca/rutas/Gen9RutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huesca.rutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen9RutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
